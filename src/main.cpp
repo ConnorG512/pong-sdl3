ca275a9c40f3cb82df112ca6507196a8fd7c742d
Connor Garey <connorgarey1999@gmail.com>
2025-03-27 20:33:20 +0000

    Correct loop for window.

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,26 @@
+#include <SDL3/SDL_events.h>
+#include <SDL3/SDL_oldnames.h>
 #include <cstdio>
 #include <unistd.h>
 
 #include "sdl_window.h"
 
-int main (int , char **) {
+int main (int, char **) {
+  bool finished_running { false };
+
+  GameWindow game_window = {"Pong", 1600, 900};
   
-  GameWindow game_window = {"Pong", 600, 900};
-  
-  sleep(5);
+  while (!finished_running) {
+    SDL_Event event;
+
+    while (SDL_PollEvent(&event)) {
+      if (event.type == SDL_EVENT_QUIT) {
+        finished_running = true;  
+      }
+    } 
+    // Game logic here: 
+
+  }
 
   return 0;
 }
