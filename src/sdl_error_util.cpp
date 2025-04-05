d54749ceb744de464ce33e2d5dd39aa0bc15e79c
Connor Garey <connorgarey1999@gmail.com>
2025-04-05 22:01:23 +0100

    Changed SDL error to assert.

--- src/sdl_error_util.cpp
+++ src/sdl_error_util.cpp
@@ -1,9 +1,7 @@
 #include "sdl_error_util.h"
+#include <cassert>
 #include <string>
 
 void SDLError::SDLPrintError(void* sdl_error, std::string source_file_path){
-  if (sdl_error == nullptr) {
-    std::cout << "SDL Error! " << "Path: " << source_file_path << " Error: " << SDL_GetError() << std::endl;
-    exit(1);
-  }
+  assert(sdl_error && "SDL Error, returned null!"); 
 }
