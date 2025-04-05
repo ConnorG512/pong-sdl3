04729689376bcad4f2aaff3518843181f9d5ad12
Connor Garey <connorgarey1999@gmail.com>
2025-04-05 20:04:04 +0100

    Centered divider.

--- src/divider.cpp
+++ src/divider.cpp
@@ -2,10 +2,10 @@
 #include <SDL3/SDL_render.h>
 
 Divider::Divider(SDL_Renderer* renderer) 
-  : m_sprite_0(800, 0, 4, 216, renderer), 
-    m_sprite_1(800, 228, 4, 216, renderer), 
-    m_sprite_2(800, 456, 4, 216, renderer), 
-    m_sprite_3(800, 684, 4, 216, renderer) {
+  : m_sprite_0(800-2, 0, 4, 216, renderer), 
+    m_sprite_1(800-2, 228, 4, 216, renderer), 
+    m_sprite_2(800-2, 456, 4, 216, renderer), 
+    m_sprite_3(800-2, 684, 4, 216, renderer) {
   }
 
 void Divider::drawDividerOnScreen() {
