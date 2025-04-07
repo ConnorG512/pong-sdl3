9120d575a4cb65b5a494b60983630b3beed0cb0c
Connor Garey <connorgarey1999@gmail.com>
2025-04-07 16:49:13 +0100

    Sprite is drawing to the screen.

--- src/main.cpp
+++ src/main.cpp
@@ -10,24 +10,15 @@
 #include <SDL3/SDL_keyboard.h>
 
 #include "game_window.h"
-#include "player.h"
 #include "sdl_error_util.h"
-#include "divider.h"
 #include "gamestate.h"
-#include "ball.h"
+#include "sprite.h"
 
 int main (int, char **) {
   bool finished_running { false };
   // Create a game window on heap memory.
   std::unique_ptr<GameWindow> game_window{ new GameWindow("Pong")};
-  // Divider 
-  std::unique_ptr<Divider> background_divider{ new Divider(game_window->m_game_renderer)};
-  // Player creation
-  std::unique_ptr<Player> player_paddle_1 { new Player(100, 300, 10, 300, game_window->m_game_renderer)};
-  std::unique_ptr<Player> player_paddle_2 { new Player(1500, 300, 10 ,300, game_window->m_game_renderer)};
-  // Ball creation 
-  std::unique_ptr<Ball> ball { new Ball(800-12, 450-12, 24, 24, game_window->m_game_renderer)};
-
+  std::unique_ptr<Sprite> sprite{ new Sprite(400, 400, 10, 10, game_window->m_game_renderer)};
   // Get SDL Keyboard state
   const bool* keyboard_state = SDL_GetKeyboardState(nullptr);
 
@@ -42,51 +33,13 @@ int main (int, char **) {
 
     // Clear the backbuffer
     SDL_RenderClear(game_window->m_game_renderer);
-    // Divider 
-    background_divider->drawDividerOnScreen();
-    // Player sprites
-    player_paddle_1->colorSprite();
-    player_paddle_2->colorSprite();
-    
-    // Ball logic
-    ball->drawSpriteOnScreen();
     
     // Exit the game with escape.
     if (keyboard_state [SDL_SCANCODE_ESCAPE]) {
       finished_running = true; 
     }
     // GAME LOGIC
-    // Start on the kickoff gamestate
-    static GameState current_gamestate = GameState::kickoff;
-    switch (current_gamestate) {
-
-      case GameState::kickoff:
-        ball->startInitialMovement(player_paddle_1->GetPlayerScore(), player_paddle_2->GetPlayerScore());
-        current_gamestate = GameState::ingame;
-        break;
-
-      case GameState::ingame:
-        // Player control
-        if ( keyboard_state [SDL_SCANCODE_W]) {
-          player_paddle_1->moveYPos();
-        }
-        if (keyboard_state [SDL_SCANCODE_S]) {
-          player_paddle_1->moveYNeg();
-        }
-        if (keyboard_state [SDL_SCANCODE_I]) {
-          player_paddle_2->moveYPos();
-        }
-        if (keyboard_state [SDL_SCANCODE_K]) {
-          player_paddle_2->moveYNeg();
-        }
-        ball->moveBall();
-
-        break;
-
-      case GameState::finished:
-        printf("Finished!");
-        break;
-    };
+    sprite->drawSpriteToScreen();
 
     // Game logic here: 
       // Present backbuffer
--- src/sprite.h
+++ src/sprite.h
@@ -3,32 +3,23 @@
 
 #include <SDL3/SDL_render.h>
 #include <SDL3/SDL_rect.h>
+#include <cstdint>
 
 class Sprite {
-  private:
+  // VARIABLES
+  protected:
   SDL_FRect m_sprite;
-  int m_color_r = 255;
-  int m_color_g = 255;
-  int m_color_b = 255;
-  int m_color_a = 255;
   SDL_Renderer* m_renderer;
+  const std::uint8_t m_color_r { 255 };
+  const std::uint8_t m_color_g { 255 };
+  const std::uint8_t m_color_b { 255 };
+  const std::uint8_t m_color_a { 255 };
   
-  // Methods
-  void createRectangle();
+  // MEMBER FUNCTIONS
   public:
-
-  void moveSpriteYPos(const float& p_movement_speed);
-  void moveSpriteYNeg(const float& p_movement_speed);
-  void moveSpriteXPos(const float& p_movement_speed);
-  void moveSpriteXNeg(const float& p_movement_speed);
-  void colorSprite();
-  void setSpritePosition(const int x_position, const int y_position);
-  float& getSpritePositionX();
-  float& getSpritePositionY();
-
-  // CONSTRUCTOR
-  // Colors will default to bright white
   Sprite(float position_x, float position_y, float size_x, float size_y, SDL_Renderer* renderer);
+  void drawSpriteToScreen() const ;
+  void setSpritePosition(const int x_position, const int y_position);
 };
 
 #endif // SPRITE_H
