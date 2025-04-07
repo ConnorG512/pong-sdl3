2c4deab0e274f4f32504d6132e7f01312327e449
Connor Garey <connorgarey1999@gmail.com>
2025-04-07 19:49:12 +0100

    Created PlayerPaddle constructor.

--- src/player_paddle.cpp
+++ src/player_paddle.cpp
@@ -1,5 +1,20 @@
 #include "player_paddle.h"
+#include <SDL3/SDL_scancode.h>
+#include <cstdint>
 
-void PlayerPaddle::movePlayer() {
-  
+PlayerPaddle::PlayerPaddle(float position_x, float position_y, float size_x, float size_y, float movement_speed, SDL_Renderer* renderer,
+    const bool* keyboard_state, std::uint8_t paddle_up_key, std::uint8_t paddle_down_key) 
+  : Sprite(position_x, position_y, size_x, size_y, movement_speed, renderer) // Calls the constructor of the base class.
+  , m_keyboard_state{ keyboard_state }
+  , m_paddle_up_key { paddle_up_key }
+  , m_paddle_down_key { paddle_down_key} {
+
+  }
+
+void PlayerPaddle::moveAndGlideSprite() {
+  if (m_keyboard_state [m_paddle_up_key]) {
+    m_sprite.y += m_movement_speed;
+  } else if (m_keyboard_state [m_paddle_down_key]) {
+    m_sprite.y -= m_movement_speed;
+  }
 }
--- src/player_paddle.h
+++ src/player_paddle.h
@@ -2,14 +2,17 @@
 #define PLAYER_PADDLE_H
 
 #include "sprite.h"
+#include <cstdint>
 class PlayerPaddle : public Sprite {
   // MEMBER VARIABLES
   protected:
     const bool* m_keyboard_state { nullptr };
-  // MEMBER FUNCTIONS
+    const std::uint8_t m_paddle_up_key {}; 
+    const std::uint8_t m_paddle_down_key {}; 
+    // MEMBER FUNCTIONS
   public:
-  PlayerPaddle();
-  void movePlayer();  
+  PlayerPaddle(const bool* keyboard_state, const std::uint8_t paddle_up_key, const std::uint8_t paddle_down_key);
+  void moveAndGlideSprite() override;   
 };
 
 #endif // !PLAYER_PADDLE_H
