81f6b8c730d5ff0dc63f041db4ed45ca2e8c26bf
Connor Garey <connorgarey1999@gmail.com>
2025-04-09 13:52:14 +0100

    game_window.cpp cleanup

--- src/game_window.cpp
+++ src/game_window.cpp
@@ -3,21 +3,16 @@
 #include <SDL3/SDL_video.h>
 #include <cassert>
 
-// Public
-// CONSTRUCTOR
 GameWindow::GameWindow(const std::string& window_title)
   : m_window_title { window_title } {
     createWindow();
     createRenderer();
   }
-// DESTRUCTOR
 GameWindow::~GameWindow() {
   SDL_DestroyWindow(m_game_window);
   SDL_DestroyRenderer(m_game_renderer);
   SDL_Quit();
 }
-
-// Private
 void GameWindow::createWindow() {
   m_game_window = SDL_CreateWindow(m_window_title.c_str(), m_window_size_x, m_window_size_y, SDL_WINDOW_KEYBOARD_GRABBED);
     assert(m_game_window && "game_window should not be null!");
