fdc99dba7094fd5224e91ca410895109d78ff14a
Connor Garey <connorgarey1999@gmail.com>
2025-04-09 13:51:19 +0100

    ball.h cleanup.

--- src/ball.h
+++ src/ball.h
@@ -3,9 +3,6 @@
 
 #include "sprite.h"
 class Ball : public Sprite {
-  //////////////////////
-  // TYPE DEFINITIONS 
-  //////////////////////
   public:
     enum class BallDirection {
         north,
@@ -18,22 +15,15 @@ class Ball : public Sprite {
         northwest,
       };
 
-  //////////////////////
-  // MEMBER VARIABLES 
-  //////////////////////
   public: 
     BallDirection m_current_direction {};
   private:
   const int m_window_width { 0 };
 
-  //////////////////////
-  // MEMBER FUNCTIONS
-  //////////////////////
   public:
     Ball(float position_x, float position_y, float size_x, float size_y, float movement_speed, SDL_Renderer* renderer,
         const int screen_width);
     void moveAndGlideSprite() override;
-  private:
 };
 
 #endif // !BALL_H 
